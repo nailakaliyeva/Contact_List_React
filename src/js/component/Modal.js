a94ddc135a9aa54293844e8837c63a45494c398a
Naila <nailakaliyeva@gmail.com>
2019-08-13 18:49:28 +0000

    Forgot to put return in modal thats why it wasnt showing

--- src/js/component/Modal.js
+++ src/js/component/Modal.js
@@ -15,42 +15,44 @@ class Modal extends React.Component {
 		return (
 			<Context.Consumer>
 				{({ store, actions }) => {
-					<div
-						className="modal"
-						tabIndex="-1"
-						role="dialog"
-						style={{ display: this.props.show ? "inline-block" : "none" }}>
-						<div className="modal-dialog" role="document">
-							<div className="modal-content">
-								<div className="modal-header">
-									<h5 className="modal-title">Are you sure?</h5>
-									{this.props.onClose ? (
-										<button
-											onClick={() => this.props.onClose()}
-											type="button"
-											className="close"
-											data-dismiss="modal"
-											aria-label="Close">
-											<span aria-hidden="true">&times;</span>
+					return (
+						<div
+							className="modal"
+							tabIndex="-1"
+							role="dialog"
+							style={{ display: this.props.show ? "inline-block" : "none" }}>
+							<div className="modal-dialog" role="document">
+								<div className="modal-content">
+									<div className="modal-header">
+										<h5 className="modal-title">Are you sure?</h5>
+										{this.props.onClose ? (
+											<button
+												onClick={() => this.props.onClose()}
+												type="button"
+												className="close"
+												data-dismiss="modal"
+												aria-label="Close">
+												<span aria-hidden="true">&times;</span>
+											</button>
+										) : (
+											""
+										)}
+									</div>
+									<div className="modal-body">
+										<p>Warning: unknown consequences after this point... Kidding!</p>
+									</div>
+									<div className="modal-footer">
+										<button type="button" className="btn btn-primary">
+											Oh no!
 										</button>
-									) : (
-										""
-									)}
-								</div>
-								<div className="modal-body">
-									<p>Warning: unknown consequences after this point... Kidding!</p>
-								</div>
-								<div className="modal-footer">
-									<button type="button" className="btn btn-primary">
-										Oh no!
-									</button>
-									<button type="button" className="btn btn-secondary" data-dismiss="modal">
-										Do it!
-									</button>
+										<button type="button" className="btn btn-secondary" data-dismiss="modal">
+											Do it!
+										</button>
+									</div>
 								</div>
 							</div>
 						</div>
-					</div>;
+					);
 				}}
 			</Context.Consumer>
 		);
