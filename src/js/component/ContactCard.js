542c3c65d1ec40eb15cf51451e2b4a797d4a5ab3
Naila <nailakaliyeva@gmail.com>
2020-12-12 06:59:12 +0000

    got rid of img tag

--- src/js/component/ContactCard.js
+++ src/js/component/ContactCard.js
@@ -14,13 +14,6 @@ class ContactCard extends React.Component {
 		return (
 			<li className="list-group-item">
 				<div className="row w-100">
-					<div className="col-12 col-sm-6 col-md-3 px-0">
-						<img
-							src="http://demos.themes.guide/bodeo/assets/images/users/m101.jpg"
-							alt={this.props.full_name}
-							className="rounded-circle mx-auto d-block img-fluid"
-						/>
-					</div>
 					<div className="col-12 col-sm-6 col-md-9 text-center text-sm-left">
 						<div className=" float-right">
 							<Link to={"/edit/" + JSON.stringify(this.props.obj)}>
